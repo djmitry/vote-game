900d7c6673c48a075b89323e36f4014b7e10c0a0
Dmitriy <dimazagainov1@gmail.com>
2022-12-11 21:58:41 +0100

    create vote form

--- assets/vue/controllers/CreateVote.vue
+++ assets/vue/controllers/CreateVote.vue
@@ -1,9 +1,88 @@
 <template>
-  <div>Create vote {{ name }}!</div>
+  <div class="alert alert-primary" v-if="message">{{ message }}</div>
+  <button @click="showForm = !showForm; message = ''" v-if="!showForm" class="btn btn-primary mb-3">Create vote</button>
+  <div class="container" v-if="showForm">
+    <div class="row">
+      <div class="col-md-4">
+        <form @submit.prevent="addVote" method="post" action="/vote/create">
+          <div class="mb-2">
+            <label for="">Title</label>
+            <input type="text" v-model="title" placeholder="Title" class="form-control">
+          </div>
+          <div class="mb-2">
+            <label for="">Description</label>
+            <input type="text" v-model="description" placeholder="Description" class="form-control">
+          </div>
+          <div class="mb-2">
+            <label for="">Bet</label>
+            <input type="number" v-model="bet" placeholder="Bet" class="form-control">
+          </div>
+          <div class="mb-2">
+            <label>BetCondition</label>
+            <div class="form-check">
+              <input type="radio" id="Like" :value="1" v-model="betCondition" class="form-check-input">
+              <label for="Like" class="form-check-label">Like</label>
+            </div>
+            <div class="form-check">
+              <input type="radio" id="Dislike" :value="2" v-model="betCondition" class="form-check-input">
+              <label for="Dislike" class="form-check-label">Dislike</label>
+            </div>
+            <div class="form-check">
+              <input type="radio" id="Equals" :value="3" v-model="betCondition" class="form-check-input">
+              <label for="Equals" class="form-check-label">Equals</label>
+            </div>
+          </div>
+          <button type="submit" class="btn btn-success">Add</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-  defineProps({
-    name: String
-  });
+
+defineProps({
+  // title: String,
+});
+</script>
+
+
+<script>
+import axios from 'axios';
+
+export default {
+  methods: {
+    addVote() {
+      axios
+          .post("/vote/create", {
+            title: this.title,
+            description: this.description,
+            bet: this.bet,
+            betCondition: this.betCondition,
+          })
+          .then((response) => {
+            console.log(response)
+            this.message = response.data.message;
+
+            if (response.data.success) {
+              this.showForm = false;
+              this.title = '';
+              this.description = '';
+              this.bet = '';
+              this.betCondition = '';
+            }
+          });
+    },
+  },
+  data() {
+    return {
+      showForm: false,
+      title: '',
+      description: '',
+      bet: '',
+      betCondition: '',
+      message: '',
+    }
+  }
+}
 </script>
--- src/Controller/VoteController.php
+++ src/Controller/VoteController.php
@@ -8,12 +8,16 @@ use App\Entity\User;
 use App\Entity\Vote;
 use App\Enum\BetCondition;
 use App\Form\BetType;
+use App\Repository\VoteRepository;
 use App\Service\VoteService;
+use DateTimeImmutable;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 #[Route(name: 'app_vote_')]
 class VoteController extends AbstractController
@@ -28,7 +32,41 @@ class VoteController extends AbstractController
         ]);
     }
 
-    #[Route('/view/{vote}', name: 'view')]
+    //TODO: factory
+    #[Route('/vote/create')]
+    public function createVote(Request $request, VoteService $voteService, VoteRepository $voteRepository): Response
+    {
+        if (!$this->isGranted('ROLE_USER')) {
+            throw new AuthenticationException();
+        }
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        try {
+            $data = json_decode($request->getContent(), true);
+            $vote = new Vote();
+            $vote->setTitle($data['title']);
+            $vote->setDescription($data['description']);
+            $vote->setBet($data['bet']);
+            $vote->setBetCondition(BetCondition::from($data['betCondition']));
+            $vote->setUser($user);
+            $date = new DateTimeImmutable();
+            $date = $date->modify('+ 5 minutes');
+            $vote->setFinishedAt($date);
+
+            $voteRepository->save($vote, true);
+            $success = true;
+            $message = 'Vote created.';
+        } catch(\Throwable) {
+            $success = false;
+            $message = 'Vote error.';
+        }
+
+        return new JsonResponse(['success' => $success, 'message' => $message]);
+    }
+
+    #[Route('/vote/view/{vote}', name: 'view')]
     public function view(Vote $vote, Request $request, VoteService $voteService): Response
     {
         if ($this->isGranted('ROLE_USER')) {
